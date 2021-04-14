2d0088591ac97914a4f437b45ceea8b41fa48521
LennartBendfeldt <46977057+LennartBendfeldt@users.noreply.github.com>
2021-04-14 15:00:09 -0400

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("Hello World");
+	AsciiChars.printUpperCase();
+	AsciiChars.printNumbers();
+	AsciiChars.printLowerCase();
+
+	UserInput.getInput();
+    }
+}
